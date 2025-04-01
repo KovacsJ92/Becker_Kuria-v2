b3d8513c02ece4d5321e800fe4621c29dc06c590
Tony Kovacs <attila.kovacs@elementary-interactive.com>
2025-04-01 22:01:37 +0200

    prices

--- resources/views/pages/room.blade.php
+++ resources/views/pages/room.blade.php
@@ -1,9 +1,13 @@
-@section('title') {{ __('Szoba') }} @endsection
+@section('title')
+    {{ __('Szoba') }}
+@endsection
 
 @section('meta_data')
-    <meta name="description" content="{{ __('A kúriában 6 darab, 22 nm-es két ágyas szoba található, melyekhez 3 nm-es zuhanyzós fürdőszoba tartozik. A feltüntetett árak az idegenforgalmi és áruforgalmi adót tartalmazzák. Az ár az étkezéseket nem tartalmazza.') }}">
-    <meta property="og:description" content="{{ __('A kúriában 6 darab, 22 nm-es két ágyas szoba található, melyekhez 3 nm-es zuhanyzós fürdőszoba tartozik. A feltüntetett árak az idegenforgalmi és áruforgalmi adót tartalmazzák. Az ár az étkezéseket nem tartalmazza.') }}">
-    <meta property="og:url" content="https://beckerkuria.hu/hu/szoba"> 
+    <meta name="description"
+        content="{{ __('A kúriában 6 darab, 22 nm-es két ágyas szoba található, melyekhez 3 nm-es zuhanyzós fürdőszoba tartozik. A feltüntetett árak az idegenforgalmi és áruforgalmi adót tartalmazzák. Az ár az étkezéseket nem tartalmazza.') }}">
+    <meta property="og:description"
+        content="{{ __('A kúriában 6 darab, 22 nm-es két ágyas szoba található, melyekhez 3 nm-es zuhanyzós fürdőszoba tartozik. A feltüntetett árak az idegenforgalmi és áruforgalmi adót tartalmazzák. Az ár az étkezéseket nem tartalmazza.') }}">
+    <meta property="og:url" content="https://beckerkuria.hu/hu/szoba">
 @endsection
 
 <x-layout>
@@ -14,23 +18,29 @@
                 <h1 class="text-gold99 text-5xl lg:text-6xl uppercase font-semibold tracking-wider">Szoba</h1>
             </div>
             <div class="flex justify-center">
-                <p class="py-10 text-3xl text-gray20 w-2/3 text-center">A kúriában 6 darab, 22 nm-es két ágyas szoba található, melyekhez 3 nm-es zuhanyzós fürdőszoba tartozik. A feltüntetett árak az idegenforgalmi és áruforgalmi adót tartalmazzák. Az ár az étkezéseket nem tartalmazza.</p>
+                <p class="py-10 text-3xl text-gray20 w-2/3 text-center">A kúriában 6 darab, 22 nm-es két ágyas szoba
+                    található, melyekhez 3 nm-es zuhanyzós fürdőszoba tartozik. A feltüntetett árak az idegenforgalmi és
+                    áruforgalmi adót tartalmazzák. Az ár az étkezéseket nem tartalmazza.</p>
             </div>
             <div class="flex justify-center pt-10 pb-20">
                 <div class="carousel-inner relative overflow-hidden w-full md:w-2/3 shadow-xl">
                     @foreach (config('gallery.rooms') as $room)
-                        
-                      <input class="carousel-open z-10 hidden" type="radio" id="carousel-{{ $loop->index }}" name="carousel" aria-hidden="true" hidden="" @if($loop->index == 0) checked="checked" @endif>
-                      <div class="carousel-item absolute opacity-0">
-                          <div class="shadow-lg">
-                              <picture class="relative">
-                                  <source srcset ="/images/{{ $room['name']}}.webp" type="image/webp">
-                                  <img class="w-full" src="/images/{{ $room['name']}}.png" alt="{{ $room['alt']}}" title="{{ $room['title']}}" height="640" width="860">
-                              </picture>
-                          </div>
-                      </div>
-                      <label for="carousel-{{ $room['label'] }}" class="prev control-{{ $loop->index }} w-16 h-16 ml-2 md:ml-10 absolute cursor-pointer hidden text-5xl font-bold text-gold99 hover:text-white rounded-full bg-white hover:bg-gold99 leading-tight text-center z-10 inset-y-0 left-0 my-auto">‹</label>
-                      <label for="carousel-{{ $room['label'] }}" class="next control-{{ $loop->index }} w-16 h-16 mr-2 md:mr-10 absolute cursor-pointer hidden text-5xl font-bold text-gold99 hover:text-white rounded-full bg-white hover:bg-gold99 leading-tight text-center z-10 inset-y-0 right-0 my-auto">›</label>
+                        <input class="carousel-open z-10 hidden" type="radio" id="carousel-{{ $loop->index }}"
+                            name="carousel" aria-hidden="true" hidden=""
+                            @if ($loop->index == 0) checked="checked" @endif>
+                        <div class="carousel-item absolute opacity-0">
+                            <div class="shadow-lg">
+                                <picture class="relative">
+                                    <source srcset ="/images/{{ $room['name'] }}.webp" type="image/webp">
+                                    <img class="w-full" src="/images/{{ $room['name'] }}.png" alt="{{ $room['alt'] }}"
+                                        title="{{ $room['title'] }}" height="640" width="860">
+                                </picture>
+                            </div>
+                        </div>
+                        <label for="carousel-{{ $room['label'] }}"
+                            class="prev control-{{ $loop->index }} w-16 h-16 ml-2 md:ml-10 absolute cursor-pointer hidden text-5xl font-bold text-gold99 hover:text-white rounded-full bg-white hover:bg-gold99 leading-tight text-center z-10 inset-y-0 left-0 my-auto">‹</label>
+                        <label for="carousel-{{ $room['label'] }}"
+                            class="next control-{{ $loop->index }} w-16 h-16 mr-2 md:mr-10 absolute cursor-pointer hidden text-5xl font-bold text-gold99 hover:text-white rounded-full bg-white hover:bg-gold99 leading-tight text-center z-10 inset-y-0 right-0 my-auto">›</label>
                     @endforeach
                 </div>
             </div>
@@ -50,7 +60,7 @@
                     </div>
                     <div class="flex flex-col items-center justify-around  space-y-5">
                         <p class="normal-case">ÁR(2fő/éj)</p>
-                        <p class="lg:text-5xl normal-case align-middle">35.000 Ft</p>
+                        <p class="lg:text-5xl normal-case align-middle">45.000 Ft</p>
                     </div>
                 </div>
             </div>
@@ -79,7 +89,8 @@
                             <p class="text-2xl lg:text-3xl text-gray20">Törölköző</p>
                         </div>
                         <div class=" flex items-center justify-start">
-                            <img class="w-10 mr-5" src="/images/svg/television.svg" alt="4k Smart TV" title="4k Smart TV">
+                            <img class="w-10 mr-5" src="/images/svg/television.svg" alt="4k Smart TV"
+                                title="4k Smart TV">
                             <p class="text-2xl lg:text-3xl text-gray20">4k Smart TV</p>
                         </div>
                     </div>
@@ -88,5 +99,5 @@
             <x-contact></x-contact>
         </x-section>
     </div>
-    
-</x-layout>    
+
+</x-layout>
--- resources/views/pages/suite.blade.php
+++ resources/views/pages/suite.blade.php
@@ -1,11 +1,15 @@
-@section('title') {{ __('Lakosztály') }} @endsection
+@section('title')
+    {{ __('Lakosztály') }}
+@endsection
 
 @section('meta_data')
-    <meta name="description" content="{{ __('A kúriában 3 darab, 35-45 nm közötti, két légteres lakosztály található, melyekhez 3-5 nm-es zuhanyzós fürdőszoba tartozik. A hálószobában kétszemélyes franciaágy, míg a nappaliban kihúzható kanapé van elhelyezve, mely maximum két, 14 éven aluli személy számára vehető igénybe. A teraszon kerti garnitúra található.
-    A feltüntetett árak az idegenforgalmi és áruforgalmi adót tartalmazzák.') }}">
-    <meta property="og:description" content="{{ __('A kúriában 3 darab, 35-45 nm közötti, két légteres lakosztály található, melyekhez 3-5 nm-es zuhanyzós fürdőszoba tartozik. A hálószobában kétszemélyes franciaágy, míg a nappaliban kihúzható kanapé van elhelyezve, mely maximum két, 14 éven aluli személy számára vehető igénybe. A teraszon kerti garnitúra található.
-    A feltüntetett árak az idegenforgalmi és áruforgalmi adót tartalmazzák.') }}">
-    <meta property="og:url" content="https://beckerkuria.hu/hu/szoba"> 
+    <meta name="description"
+        content="{{ __('A kúriában 3 darab, 35-45 nm közötti, két légteres lakosztály található, melyekhez 3-5 nm-es zuhanyzós fürdőszoba tartozik. A hálószobában kétszemélyes franciaágy, míg a nappaliban kihúzható kanapé van elhelyezve, mely maximum két, 14 éven aluli személy számára vehető igénybe. A teraszon kerti garnitúra található.
+            A feltüntetett árak az idegenforgalmi és áruforgalmi adót tartalmazzák.') }}">
+    <meta property="og:description"
+        content="{{ __('A kúriában 3 darab, 35-45 nm közötti, két légteres lakosztály található, melyekhez 3-5 nm-es zuhanyzós fürdőszoba tartozik. A hálószobában kétszemélyes franciaágy, míg a nappaliban kihúzható kanapé van elhelyezve, mely maximum két, 14 éven aluli személy számára vehető igénybe. A teraszon kerti garnitúra található.
+            A feltüntetett árak az idegenforgalmi és áruforgalmi adót tartalmazzák.') }}">
+    <meta property="og:url" content="https://beckerkuria.hu/hu/szoba">
 @endsection
 
 <x-layout>
@@ -16,24 +20,32 @@
                 <h1 class="text-gold99 text-5xl lg:text-6xl uppercase font-semibold tracking-wider">Lakosztály</h1>
             </div>
             <div class="flex justify-center">
-                <p class="py-10 text-3xl text-gray20 w-2/3 text-center">A kúriában 3 darab, 35-45 nm közötti, két légteres lakosztály található, melyekhez 3-5 nm-es zuhanyzós fürdőszoba tartozik. A hálószobában kétszemélyes franciaágy, míg a nappaliban kihúzható kanapé van elhelyezve, mely maximum két, 14 éven aluli személy számára vehető igénybe. A teraszon kerti garnitúra található.
-                    A feltüntetett árak az idegenforgalmi és áruforgalmi adót tartalmazzák. Az ár az étkezéseket nem tartalmazza.</p>
+                <p class="py-10 text-3xl text-gray20 w-2/3 text-center">A kúriában 3 darab, 35-45 nm közötti, két
+                    légteres lakosztály található, melyekhez 3-5 nm-es zuhanyzós fürdőszoba tartozik. A hálószobában
+                    kétszemélyes franciaágy, míg a nappaliban kihúzható kanapé van elhelyezve, mely maximum két, 14 éven
+                    aluli személy számára vehető igénybe. A teraszon kerti garnitúra található.
+                    A feltüntetett árak az idegenforgalmi és áruforgalmi adót tartalmazzák. Az ár az étkezéseket nem
+                    tartalmazza.</p>
             </div>
             <div class="flex justify-center pt-10 pb-20">
                 <div class="carousel-inner relative overflow-hidden w-full md:w-2/3 shadow-xl">
                     @foreach (config('gallery.suites') as $suite)
-                        
-                      <input class="carousel-open z-10 hidden" type="radio" id="carousel-{{ $loop->index }}" name="carousel" aria-hidden="true" hidden="" @if($loop->index == 0) checked="checked" @endif>
-                      <div class="carousel-item absolute opacity-0">
-                          <div class="shadow-lg">
-                              <picture class="relative">
-                                  <source srcset ="/images/{{ $suite['name']}}.webp" type="image/webp">
-                                  <img class="w-full" src="/images/{{ $suite['name']}}.png" alt="{{ $suite['alt']}}" title="{{ $suite['title']}}" height="640" width="860">
-                              </picture>
-                          </div>
-                      </div>
-                      <label for="carousel-{{ $suite['label'] }}" class="prev control-{{ $loop->index }} w-16 h-16 ml-2 md:ml-10 absolute cursor-pointer hidden text-5xl font-bold text-gold99 hover:text-white rounded-full bg-white hover:bg-gold99 leading-tight text-center z-10 inset-y-0 left-0 my-auto">‹</label>
-                      <label for="carousel-{{ $suite['label'] }}" class="next control-{{ $loop->index }} w-16 h-16 mr-2 md:mr-10 absolute cursor-pointer hidden text-5xl font-bold text-gold99 hover:text-white rounded-full bg-white hover:bg-gold99 leading-tight text-center z-10 inset-y-0 right-0 my-auto">›</label>
+                        <input class="carousel-open z-10 hidden" type="radio" id="carousel-{{ $loop->index }}"
+                            name="carousel" aria-hidden="true" hidden=""
+                            @if ($loop->index == 0) checked="checked" @endif>
+                        <div class="carousel-item absolute opacity-0">
+                            <div class="shadow-lg">
+                                <picture class="relative">
+                                    <source srcset ="/images/{{ $suite['name'] }}.webp" type="image/webp">
+                                    <img class="w-full" src="/images/{{ $suite['name'] }}.png" alt="{{ $suite['alt'] }}"
+                                        title="{{ $suite['title'] }}" height="640" width="860">
+                                </picture>
+                            </div>
+                        </div>
+                        <label for="carousel-{{ $suite['label'] }}"
+                            class="prev control-{{ $loop->index }} w-16 h-16 ml-2 md:ml-10 absolute cursor-pointer hidden text-5xl font-bold text-gold99 hover:text-white rounded-full bg-white hover:bg-gold99 leading-tight text-center z-10 inset-y-0 left-0 my-auto">‹</label>
+                        <label for="carousel-{{ $suite['label'] }}"
+                            class="next control-{{ $loop->index }} w-16 h-16 mr-2 md:mr-10 absolute cursor-pointer hidden text-5xl font-bold text-gold99 hover:text-white rounded-full bg-white hover:bg-gold99 leading-tight text-center z-10 inset-y-0 right-0 my-auto">›</label>
                     @endforeach
                 </div>
             </div>
@@ -53,7 +65,7 @@
                     </div>
                     <div class="flex flex-col items-center justify-around  space-y-5">
                         <p class="normal-case">ÁR(2fő/éj)</p>
-                        <p class="lg:text-5xl normal-case align-middle">50.000 Ft</p>
+                        <p class="lg:text-5xl normal-case align-middle">65.000 Ft</p>
                     </div>
                 </div>
             </div>
@@ -82,31 +94,38 @@
                             <p class="text-2xl lg:text-3xl text-gray20">Törölköző</p>
                         </div>
                         <div class=" flex items-center justify-start">
-                            <img class="w-10 mr-5" src="/images/svg/television.svg" alt="4k Smart TV" title="4k Smart TV">
+                            <img class="w-10 mr-5" src="/images/svg/television.svg" alt="4k Smart TV"
+                                title="4k Smart TV">
                             <p class="text-2xl lg:text-3xl text-gray20">4k Smart TV</p>
                         </div>
                         <div class=" flex items-center justify-start ">
-                            <img class="w-10 mr-5 text-gold99" src="/images/svg/electric-kettle.svg" alt="Vízforraló" title="Vízforraló">
+                            <img class="w-10 mr-5 text-gold99" src="/images/svg/electric-kettle.svg" alt="Vízforraló"
+                                title="Vízforraló">
                             <p class="text-2xl lg:text-3xl text-gray20">Vízforraló</p>
                         </div>
                         <div class=" flex items-center justify-start ">
-                            <img class="w-10 mr-5 text-gold99" src="/images/svg/coffee-cup.svg" alt="Kapszulás kávéfőző" title="Kapszulás kávéfőző">
+                            <img class="w-10 mr-5 text-gold99" src="/images/svg/coffee-cup.svg" alt="Kapszulás kávéfőző"
+                                title="Kapszulás kávéfőző">
                             <p class="text-2xl lg:text-3xl text-gray20">Kapszulás kávéfőző</p>
                         </div>
                         <div class=" flex items-center justify-start ">
-                            <img class="w-10 mr-5 text-gold99" src="/images/svg/toaster.svg" alt="Pirítós sütő" title="Pirítós sütő">
+                            <img class="w-10 mr-5 text-gold99" src="/images/svg/toaster.svg" alt="Pirítós sütő"
+                                title="Pirítós sütő">
                             <p class="text-2xl lg:text-3xl text-gray20">Pirítós sütő</p>
                         </div>
                         <div class=" flex items-center justify-start ">
-                            <img class="w-10 mr-5 text-gold99" src="/images/svg/fridge.svg" alt="Hűtőszekrény" title="Hűtőszekrény">
+                            <img class="w-10 mr-5 text-gold99" src="/images/svg/fridge.svg" alt="Hűtőszekrény"
+                                title="Hűtőszekrény">
                             <p class="text-2xl lg:text-3xl text-gray20">Hűtőszekrény</p>
                         </div>
                         <div class=" flex items-center justify-start ">
-                            <img class="w-10 mr-5 text-gold99" src="/images/svg/restaurant.svg" alt="Tányérok, evőeszközök" title="Tányérok, evőeszközök">
+                            <img class="w-10 mr-5 text-gold99" src="/images/svg/restaurant.svg"
+                                alt="Tányérok, evőeszközök" title="Tányérok, evőeszközök">
                             <p class="text-2xl lg:text-3xl text-gray20">Tányérok, evőeszközök</p>
                         </div>
                         <div class=" flex items-center justify-start ">
-                            <img class="w-10 mr-5 text-gold99" src="/images/svg/wine-glass.svg" alt="Poharak" title="Poharak">
+                            <img class="w-10 mr-5 text-gold99" src="/images/svg/wine-glass.svg" alt="Poharak"
+                                title="Poharak">
                             <p class="text-2xl lg:text-3xl text-gray20">Poharak</p>
                         </div>
                     </div>
@@ -115,5 +134,6 @@
             <x-contact></x-contact>
         </x-section>
     </div>
-    
+
 </x-layout>
+
